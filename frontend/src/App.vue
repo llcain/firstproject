69a9dcd0064acbb7537cd862cb0061e0aedf83b9
llcain <llcain2012@gmail.com>
2025-11-09 14:53:58 -0600

    styled h1 - added an input and button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,11 +4,15 @@
 
 <template lang="pug">
 
-    h1 To Do List
-    p This is my To Do List built using Python!
+   h1 To Do List
+   input(type='input' name='Enter task')
+   button Add
 
 </template>
 
 <style scoped>
+    h1 {
+    color: green;
+    }
 
 </style>
